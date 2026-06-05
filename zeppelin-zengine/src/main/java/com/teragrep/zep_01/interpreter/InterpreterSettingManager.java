b4bec26526f386b5fb40c926202d75defc1af4fc
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-06-05 10:22:33 +0300

    Removed unused logging

--- zeppelin-zengine/src/main/java/com/teragrep/zep_01/interpreter/InterpreterSettingManager.java
+++ zeppelin-zengine/src/main/java/com/teragrep/zep_01/interpreter/InterpreterSettingManager.java
@@ -892,10 +892,6 @@ public class InterpreterSettingManager implements NoteEventListener {
   }
 
   public InterpreterSetting get(String id) {
-    LOGGER.info("p52u current interpreterSettingIDs:"); //TODO: revert
-    for (InterpreterSetting setting:interpreterSettings.values()) {
-      LOGGER.info(setting.getId());
-    }
     return interpreterSettings.get(id);
   }
 
