64f75a64261d83d72986683ed413d3769e58a105
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-06-10 13:02:06 +0300

    Changed ExceptionType of NoteNodeStub from RuntimeException

--- zeppelin-zengine/src/main/java/com/teragrep/zep_01/notebook/NoteNodeStub.java
+++ zeppelin-zengine/src/main/java/com/teragrep/zep_01/notebook/NoteNodeStub.java
@@ -6,52 +6,52 @@ public final class NoteNodeStub implements NoteNode {
 
     @Override
     public Note getNote() throws IOException {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public Note getNote(final boolean reload) throws IOException {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public String getNoteId() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public String getNoteName() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public String getNotePath() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public Note getRawNote() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public Folder getParent() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public void setParent(final Folder parent) {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public void setNotePath(final String notePath) {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
     public void updateNotePath() {
-        throw new RuntimeException("NoteNode is a stub!");
+        throw new UnsupportedOperationException("NoteNode is a stub!");
     }
 
     @Override
