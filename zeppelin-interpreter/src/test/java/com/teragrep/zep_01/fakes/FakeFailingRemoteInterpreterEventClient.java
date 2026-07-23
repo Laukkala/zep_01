a0ea1303459fc1374a6428b45a28525bc06357c2
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-07-23 11:49:26 +0300

    Made Fakes final, removed unused assignment, added equals() and hashcode()

--- zeppelin-interpreter/src/test/java/com/teragrep/zep_01/fakes/FakeFailingRemoteInterpreterEventClient.java
+++ zeppelin-interpreter/src/test/java/com/teragrep/zep_01/fakes/FakeFailingRemoteInterpreterEventClient.java
@@ -2,20 +2,33 @@ package com.teragrep.zep_01.fakes;
 
 import com.teragrep.zep_01.interpreter.remote.RemoteInterpreterEventClient;
 
+import java.util.Objects;
+
 public final class FakeFailingRemoteInterpreterEventClient extends RemoteInterpreterEventClient {
-    private final RuntimeException expectedException;
-    private RuntimeException thrownException;
-    public FakeFailingRemoteInterpreterEventClient(final String host, final int port, final int connectionPoolSize, RuntimeException expectedException){
+    private final RuntimeException exception;
+    public FakeFailingRemoteInterpreterEventClient(final String host, final int port, final int connectionPoolSize, final RuntimeException exception){
         super(host,port,connectionPoolSize);
-        this.expectedException =expectedException;
+        this.exception = exception;
     }
 
     @Override
     public void unRegisterInterpreterProcess() {
-        thrownException = expectedException;
-        throw thrownException;
+        throw exception;
     }
     public RuntimeException exception(){
-        return thrownException;
+        return exception;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FakeFailingRemoteInterpreterEventClient that = (FakeFailingRemoteInterpreterEventClient) o;
+        return Objects.equals(exception, that.exception);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(exception);
     }
 }
--- zeppelin-interpreter/src/test/java/com/teragrep/zep_01/fakes/FakeRemoteInterpreterEventClient.java
+++ zeppelin-interpreter/src/test/java/com/teragrep/zep_01/fakes/FakeRemoteInterpreterEventClient.java
@@ -2,6 +2,8 @@ package com.teragrep.zep_01.fakes;
 
 import com.teragrep.zep_01.interpreter.remote.RemoteInterpreterEventClient;
 
+import java.util.Objects;
+
 public final class FakeRemoteInterpreterEventClient extends RemoteInterpreterEventClient {
     private boolean unregistered = false;
     public FakeRemoteInterpreterEventClient(final String host, final int port, final int connectionPoolSize){
@@ -14,5 +16,18 @@ public final class FakeRemoteInterpreterEventClient extends RemoteInterpreterEve
     }
     public boolean unregistered(){
         return unregistered;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FakeRemoteInterpreterEventClient that = (FakeRemoteInterpreterEventClient) o;
+        return unregistered == that.unregistered;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(unregistered);
     }
 }
