0efd1bd0013bbd362c61f264b1b95f2f8eb9c956
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-08-11 10:19:26 +0300

    Moved Dataset transformation related methods from UPlotFormat to a new object

--- spark/dpl-interpreter/src/main/java/com/teragrep/pth_07/ui/elements/table_dynamic/formats/UPlotFormat.java
+++ spark/dpl-interpreter/src/main/java/com/teragrep/pth_07/ui/elements/table_dynamic/formats/UPlotFormat.java
@@ -65,8 +65,8 @@ public final class UPlotFormat implements RenderFormat{
      */
 
     public UPlotFormat(final UIOption option, final Dataset<Row> rowDataset){
-        this.dataset = rowDataset;
         this.option = option;
+        this.dataset = rowDataset;
     }
 
     @Override
@@ -103,15 +103,9 @@ public final class UPlotFormat implements RenderFormat{
                 }
             }
         }
-        final Dataset<Row> concatenatedDataset = concatenateGroupByColumns(dataset,xAxisColumnNames);
-        final Dataset<Row> transformedDataset;
-        if(!groupByColumnNames.isEmpty()){
-            groupByColumnNames.add("label");
-            transformedDataset = pivotByColumns(concatenatedDataset,groupByColumnNames,valueColumnNames);
-        }
-        else {
-            transformedDataset = concatenatedDataset;
-        }
+
+        final UPlotDatasetTransformation transformation = new UPlotDatasetTransformation(dataset, xAxisColumnNames, groupByColumnNames, valueColumnNames);
+        final Dataset<Row> transformedDataset = transformation.apply();
 
         final List<Row> rows = transformedDataset.collectAsList();
         final String graphType = requestOptions.getString("graphType");
@@ -125,84 +119,6 @@ public final class UPlotFormat implements RenderFormat{
                 .add("isAggregated",uPlotMetadata.isAggregated())
                 .add("type", InterpreterResult.Type.UPLOT.label);
         return builder.build();
-    }
-
-    /**
-     * Creates a appropriately transformed dataset when a dataset's X-axis should be a timescale. Dataset should contain a column named "_time"
-     * This method pivots the dataset so that only "_time" is used as the X-axis, and combinations of unique values in other group by columns are combined into different series on the Y-axis.
-     * @param dataset The dataset to transform
-     * @param groupByColumnNames List of column names used in group by clauses
-     * @param valueColumnNames List of column names used outside of group by clauses
-     * @return A transformed dataset ready for formatting.
-     */
-    private Dataset<Row> pivotByColumns(final Dataset<Row> dataset, final List<String> groupByColumnNames, final List<String> valueColumnNames){
-        final Dataset<Row> transformedDataset;
-        if(groupByColumnNames.size() < 2){
-            transformedDataset = dataset;
-        }
-        else {
-            // Grouping columns as Column array for transformation
-            final List<String> timechartGroupByColumnNames = new ArrayList<>(groupByColumnNames);
-            timechartGroupByColumnNames.remove("label");
-
-            final List<Column> columns = new ArrayList<>();
-            for (int i = 0; i < valueColumnNames.size(); i++) {
-                columns.add(org.apache.spark.sql.functions.first(valueColumnNames.get(i)).alias(valueColumnNames.get(i)));
-            }
-            final Column first = columns.get(0);
-            columns.remove(0);
-            final Column[] rest = columns.toArray(new Column[0]);
-
-            final List<Column> groupByColumns = new ArrayList<>();
-            for (int i = 0; i < timechartGroupByColumnNames.size(); i++) {
-                groupByColumns.add(org.apache.spark.sql.functions.col(timechartGroupByColumnNames.get(i)));
-            }
-            final Column[] groupByColumnArray = groupByColumns.toArray(new Column[0]);
-            Dataset<Row> pivotedDataset = dataset
-                    .withColumn("pivot", org.apache.spark.sql.functions.concat_ws(".",groupByColumnArray))
-                    .groupBy("label")
-                    .pivot("pivot")
-                    .agg(first,rest);
-
-            // pivot() uses an underscore as the separator when it creates new columns, and it cannot be overridden. To use "." as separator we have to rename the columns.
-            for (final StructField column : pivotedDataset.schema().fields()) {
-                for (final String valueColumn : valueColumnNames){
-                    if(column.name().endsWith("_"+valueColumn)){
-                        final String existingName = column.name();
-                        final int separatorIndex = existingName.indexOf("_"+valueColumn);
-                        final StringBuilder newName = new StringBuilder(existingName);
-                        newName.setCharAt(separatorIndex,'.');
-                        pivotedDataset = pivotedDataset.withColumnRenamed(existingName, newName.toString());
-                    }
-                }
-            }
-            transformedDataset = pivotedDataset;
-        }
-        return transformedDataset;
-    }
-
-    /**
-     * Creates a transformed dataset when a dataset's X-axis should consist of the combinations of all used grouping labels.
-     * This method transforms the dataset so that
-     * @param dataset Dataset to transform
-     * @param groupByColumnNames List of names used in group by clauses
-     * @return A new dataset ready for formatting.
-     */
-
-    private Dataset<Row> concatenateGroupByColumns(final Dataset<Row> dataset, final List<String> groupByColumnNames){
-        final Dataset<Row> transformedDataset;
-        // If trying to concatenate less than two columns, we don't need to do any transformations to the data,
-            // Get columns that were used in grouping of data. These will be concatenated to a new column and then dropped.
-            final List<Column> groupByColumns = new ArrayList<>();
-            for (final String columnName:groupByColumnNames) {
-                groupByColumns.add(dataset.col(columnName));
-            }
-
-            // Create a Dataset containing the concatenated groupBy column. Copy metadata as well since it's being used later when creating labels.
-            transformedDataset = dataset.withColumn("label",functions.concat_ws(".",groupByColumns.toArray(new Column[]{})))
-                    .drop(groupByColumnNames.toArray(new String[0]))
-                    .withMetadata("label",new MetadataBuilder().putBoolean("dpl_internal_isGroupByColumn",true).build());
-        return transformedDataset;
     }
 
     public InterpreterResult.Type type(){
