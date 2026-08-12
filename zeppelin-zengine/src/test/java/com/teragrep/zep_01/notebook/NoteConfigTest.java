9bd8415b300dca8958eeab3a8c380a6f757f35cc
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-08-12 14:45:16 +0300

    Added tests for invalid and missing keys for Jsonables that use non-String values

--- zeppelin-zengine/src/test/java/com/teragrep/zep_01/notebook/NoteConfigTest.java
+++ zeppelin-zengine/src/test/java/com/teragrep/zep_01/notebook/NoteConfigTest.java
@@ -12,7 +12,7 @@ import java.util.Map;
 public final class NoteConfigTest {
 
     @Test
-    public void asJson(){
+    public void testAsJson(){
         final String bodyClassName = "testName";
         final String cronInput = "testCron";
         final boolean cronEnabled = true;
@@ -34,8 +34,77 @@ public final class NoteConfigTest {
         Assertions.assertEquals(looknfeel,json.getString("looknfeel"));
         Assertions.assertEquals(personalizedMode,json.getString("personalizedMode"));
     }
-    @org.junit.jupiter.api.Test
-    void equalsVerifier() {
+
+    @Test
+    public void testMissingKeys(){
+        final String cronInput = "testCron";
+        final boolean cronEnabled = true;
+
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("cronInput", cronInput);
+        configMap.put("isZeppelinNotebookCronEnable", cronEnabled);
+
+        final NoteConfig config = new NoteConfig(configMap);
+        final JsonObject json = config.asJson();
+        Assertions.assertEquals(cronInput,json.getString("cronInput"));
+        Assertions.assertEquals(cronEnabled,json.getBoolean("isZeppelinNotebookCronEnable"));
+
+        // Missing keys should be omitted, valid keys should exist
+        Assertions.assertEquals(2,json.size());
+        Assertions.assertEquals(cronInput,json.getString("cronInput"));
+        Assertions.assertEquals(cronEnabled,json.getBoolean("isZeppelinNotebookCronEnable"));
+    }
+
+    @Test
+    public void testInvalidValues(){
+        final boolean bodyClassName = true;
+        final long cronInput = 100l;
+        final int invalidCronEnabled = 500;
+        final String looknfeel = "testLookNFeel";
+        final String personalizedMode = "testPersonalizedMode";
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("bodyClassName", bodyClassName);
+        configMap.put("cronInput", cronInput);
+        configMap.put("isZeppelinNotebookCronEnable", invalidCronEnabled);
+        configMap.put("looknfeel", looknfeel);
+        configMap.put("personalizedMode", personalizedMode);
+
+        // Keys with invalid values should be omitted, keys with valid values should exist
+        final NoteConfig config = new NoteConfig(configMap);
+        final JsonObject json = config.asJson();
+        Assertions.assertEquals(4, json.size());
+        Assertions.assertEquals("true",json.getString("bodyClassName"));
+        Assertions.assertEquals("100",json.getString("cronInput"));
+        Assertions.assertFalse(json.containsKey("isZeppelinNotebookCronEnable"));
+        Assertions.assertEquals(looknfeel,json.getString("looknfeel"));
+        Assertions.assertEquals(personalizedMode,json.getString("personalizedMode"));
+
+    }
+
+    @Test
+    public void testInvalidKeys(){
+        final String bodyClassName = "testName";
+        final String cronInput = "testCron";
+        final boolean cronEnabled = true;
+        final String looknfeel = "testLookNFeel";
+        final String personalizedMode = "testPersonalizedMode";
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("invalidBodyClassName", bodyClassName);
+        configMap.put("invalidCronInput", cronInput);
+        configMap.put("invalidIsZeppelinNotebookCronEnable", cronEnabled);
+        configMap.put("looknfeel", looknfeel);
+        configMap.put("personalizedMode", personalizedMode);
+
+        // Invalid keys should be omitted, valid keys should exist
+        final NoteConfig config = new NoteConfig(configMap);
+        final JsonObject json = config.asJson();
+        Assertions.assertEquals(2, json.size());
+        Assertions.assertEquals(looknfeel,json.getString("looknfeel"));
+        Assertions.assertEquals(personalizedMode,json.getString("personalizedMode"));
+    }
+
+    @Test
+    public void equalsVerifier() {
         EqualsVerifier.forClass(NoteConfig.class).verify();
     }
 }
--- zeppelin-zengine/src/test/java/com/teragrep/zep_01/notebook/ParagraphConfigTest.java
+++ zeppelin-zengine/src/test/java/com/teragrep/zep_01/notebook/ParagraphConfigTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public final class ParagraphConfigTest {
 
     @Test
-    void asJson() {
+    public void testAsJson() {
         final int colWidth = 12;
         final boolean enabled = true;
         final int fontSize = 12;
@@ -51,6 +51,80 @@ public final class ParagraphConfigTest {
         Assertions.assertEquals(completionSupport, editorSettingJson.getBoolean("completionSupport"));
         Assertions.assertEquals(editOnDoubleClick, editorSettingJson.getBoolean("editOnDoubleClick"));
     }
+
+    @Test
+    public void testMissingKeys() {
+        final boolean enabled = true;
+        final int fontSize = 12;
+        final boolean lineNumbers = true;
+
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("enabled",enabled);
+        configMap.put("fontSize",fontSize);
+        configMap.put("lineNumbers",lineNumbers);
+        final ParagraphConfig config = new ParagraphConfig(configMap);
+        final JsonObject json = config.asJson();
+
+        // Missing keys should be omitted, valid keys should still exist
+        Assertions.assertEquals(3, json.size());
+        Assertions.assertEquals(enabled,json.getBoolean("enabled"));
+        Assertions.assertEquals(fontSize,json.getInt("fontSize"));
+        Assertions.assertEquals(lineNumbers,json.getBoolean("lineNumbers"));
+    }
+
+    @Test
+    public void testInvalidKeys() {
+        final int colWidth = 12;
+        final boolean enabled = true;
+        final int fontSize = 12;
+        final boolean lineNumbers = true;
+        final String editorMode = "mode";
+
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("colWidth",colWidth);
+        configMap.put("enabled",enabled);
+        configMap.put("fontSize",fontSize);
+        configMap.put("invalidLineNumbers",lineNumbers);
+        configMap.put("invalidEditorMode",editorMode);
+        final ParagraphConfig config = new ParagraphConfig(configMap);
+        final JsonObject json = config.asJson();
+
+        // Invalid keys should be omitted, valid keys should still exist
+        Assertions.assertEquals(3, json.size());
+        Assertions.assertEquals(colWidth,json.getInt("colWidth"));
+        Assertions.assertEquals(enabled,json.getBoolean("enabled"));
+        Assertions.assertEquals(fontSize,json.getInt("fontSize"));
+        Assertions.assertFalse(json.containsKey("lineNumbers"));
+        Assertions.assertFalse(json.containsKey("editorMode"));
+    }
+
+    @Test
+    public void testInvalidValues() {
+        final String invalidColumnWidth = "seventeen";
+        final long invalidEnabled = 100l;
+        final boolean invalidFontSize = false;
+        final boolean lineNumbers = true;
+        final String editorMode = "mode";
+
+
+        final Map<String, Object> configMap = new HashMap<>();
+        configMap.put("colWidth",invalidColumnWidth);
+        configMap.put("enabled",invalidEnabled);
+        configMap.put("fontSize",invalidFontSize);
+        configMap.put("lineNumbers",lineNumbers);
+        configMap.put("editorMode",editorMode);
+        final ParagraphConfig config = new ParagraphConfig(configMap);
+        final JsonObject json = config.asJson();
+
+        // Keys with invalid values should be omitted, keys with valid values should still exist
+        Assertions.assertEquals(2, json.size());
+        Assertions.assertFalse(json.containsKey("colWidth"));
+        Assertions.assertFalse(json.containsKey("enabled"));
+        Assertions.assertFalse(json.containsKey("fontSize"));
+        Assertions.assertEquals(lineNumbers,json.getBoolean("lineNumbers"));
+        Assertions.assertEquals(editorMode,json.getString("editorMode"));
+    }
+
     @Test
     void equalsVerifier() {
         EqualsVerifier.forClass(ParagraphConfig.class).verify();
