6182d61bd869804de2a1316d4c5bf3fd19a49b3d
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-08-19 09:58:48 +0300

    Revert "Issue 111 - test trigger"
    This reverts commit d6a6462b99b68d2f7838ab77f8fcb101e2d02c01.

--- zeppelin-server/src/main/java/com/teragrep/zep_01/socket/NotebookServer.java
+++ zeppelin-server/src/main/java/com/teragrep/zep_01/socket/NotebookServer.java
@@ -364,7 +364,6 @@ public class NotebookServer extends WebSocketServlet
           break;
         case PARAGRAPH_CLEAR_OUTPUT:
           clearParagraphOutput(conn, context, receivedMessage);
-          interpreterStatus(conn, context, receivedMessage);
           break;
         case PARAGRAPH_CLEAR_ALL_OUTPUT:
           clearAllParagraphOutput(conn, context, receivedMessage);
@@ -1169,8 +1168,8 @@ public class NotebookServer extends WebSocketServlet
                                      ServiceContext context,
                                      Message fromMessage) throws IOException, InterpreterException {
     // Casting is required to get Message parameters in correct format, as GSON parses all numbers as Doubles, and Message.get() returns a generic Object.
-    final String paragraphId = (String) fromMessage.get("id");
-    final String noteId = getConnectionManager().getAssociatedNoteId(conn);
+    final String noteId = (String) fromMessage.get("noteId");
+    final String paragraphId = (String) fromMessage.get("paragraphId");
 
     Note note = getNotebook().getNote(noteId);
     if(note == null){
